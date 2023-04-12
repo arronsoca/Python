c6f55a6438b10f7584c0dba367746345351a306a
arron soca <arronsoca@gmail.com>
2023-04-12 11:03:26 +0700

    fixing

--- age2.py
+++ age2.py
@@ -6,10 +6,10 @@ bulan = int(input("Bulan : "))
 tahun = int(input("Tahun : "))
 
 date = dt.date(tahun,bulan,tanggal)
-result = today-date
-years = (result.days // 365)
-month = (result.days % 365) // 30
-day = (result.days % 365) % 30
+age = today-date
+years = (age.days // 365)
+month = (age.days % 365) // 30
+day = (age.days % 365) % 30
 
 print(f"Lahir di hari : {date:%A}")
 print(f"umur anda adalah: {years} tahun, {month} bulan, {day} hari")
