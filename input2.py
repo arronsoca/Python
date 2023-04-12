d2707d9462f41e4196484a2b271cb87212a1fc8b
arron soca <arronsoca@gmail.com>
2023-04-12 08:47:54 +0700

    add input2

---
+++ input2.py
@@ -0,0 +1,8 @@
+name = input("Siapa namamu? ")
+hobby = input("Apa hobi kamu? ")
+hobi = "Wahh hobi kamu keren!"
+result = (name)+" hobi kamu adalah "+(hobby)
+
+print("Hello",name)
+print(hobi)
+print(result)
